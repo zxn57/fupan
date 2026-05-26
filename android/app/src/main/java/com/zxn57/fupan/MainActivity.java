8c837cbdf479fd9faef6c5b689308105c10b7c85
zxn57 <2838568549@qq.com>
2026-05-26 10:43:58 +0800

    修复：APP 文件导出原生保存 + 文件选择器回调

--- android/app/src/main/java/com/zxn57/fupan/MainActivity.java
+++ android/app/src/main/java/com/zxn57/fupan/MainActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.DownloadListener;
 import android.view.KeyEvent;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import java.io.File;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -36,10 +41,11 @@ public class MainActivity extends AppCompatActivity {
         webView.getSettings().setLoadWithOverviewMode(true);
         webView.getSettings().setMixedContentMode(0);
 
+        webView.addJavascriptInterface(new AndroidBridge(), "AndroidBridge");
+
         webView.setWebViewClient(new WebViewClient());
 
         webView.setWebChromeClient(new WebChromeClient() {
-            // Android 5.0+
             @TargetApi(Build.VERSION_CODES.LOLLIPOP)
             @Override
             public boolean onShowFileChooser(WebView wv, ValueCallback<Uri[]> callback, FileChooserParams params) {
@@ -56,7 +62,32 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        webView.setDownloadListener(new DownloadListener() {
+            @Override
+            public void onDownloadStart(String url, String userAgent, String contentDisposition, String mimeType, long contentLength) {
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+
         webView.loadUrl(APP_URL);
+    }
+
+    public class AndroidBridge {
+        @android.webkit.JavascriptInterface
+        public void saveFile(String fileName, String content) {
+            try {
+                File dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS);
+                File file = new File(dir, fileName);
+                FileOutputStream fos = new FileOutputStream(file);
+                fos.write(content.getBytes());
+                fos.close();
+                Toast.makeText(MainActivity.this, "已导出到下载目录：" + fileName, Toast.LENGTH_LONG).show();
+            } catch (Exception e) {
+                Toast.makeText(MainActivity.this, "导出失败：" + e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
